1db6f1edf5ba5e82cf54f6abb19cf3b4e6eecf56
elmysf <elmysufiandy09@gmail.com>
2023-09-14 19:43:05 +0700

    Initial Commit

---
+++ ChatQ/ChatQApp.swift
@@ -0,0 +1,17 @@
+//
+//  ChatQApp.swift
+//  ChatQ
+//
+//  Created by Sufiandy Elmy on 14/09/23.
+//
+
+import SwiftUI
+
+@main
+struct ChatQApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
